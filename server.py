676cac5d29b844a22f4590e57638153ca913019d
Phanaty <38048498+Phanaty@users.noreply.github.com>
2018-11-08 12:11:26 +0300

    server

--- server.py
+++ server.py
@@ -4,18 +4,22 @@ try:
     print("подключился к базе данных")
 except:
     print("не подключился к базе данных")
-a = str(input("Введите имя для поиска "))
+#a = str(input("Введите count "))
+#b = str(input("Введите domain"))
+#c = str(input("Введите name"))
+#d = str(input("Введите username"))
 cur = conn.cursor()
-res = cur.execute("SELECT * FROM users domain where lower(domain) like lower('%Desmond%')") #поиск информации
-#res = cur.execute("SELECT * FROM users")
+#res = cur.execute("SELECT * FROM users domain where lower(domain) like lower('%Desmond%')") #поиск информации
+res = cur.execute("SELECT * FROM users")
 row = cur.fetchone()
-print(row)        # Вывод информации с мансами
-for entry in cur: #
-    print(entry)  #
 
 #print(row) #простой вывод
 
-#res = cur.execute("INSERT INTO users(count ,domain ,name ,surname) VALUES (%s,%s,%s,%s)", ("2", "DESMOND", "ARTUR", "Youzefovich"))  #Добавление информации
+#res = cur.execute("INSERT INTO users(count ,domain ,name ,surname) VALUES (%s,%s,%s,%s)", (a, b, c, d))  #Добавление информации
 #conn.commit()
+#row = cur.fetchone()
+print(row)        # Вывод информации с мансами
+for entry in cur:
+    print(entry)
 
 conn.close() # Разрываем подключение.
