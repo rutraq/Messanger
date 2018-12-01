ea0e71d13d3c2ef0d2ddeb39562cbf911928b7a1
Phanaty <38048498+Phanaty@users.noreply.github.com>
2018-12-01 16:38:54 +0300

    шифр

--- main.py
+++ main.py
@@ -262,9 +262,6 @@ class Mainform(QtWidgets.QMainWindow, mainform.Ui_Dialog):
         if not row:
             (pubkey, privkey) = rsa.newkeys(512)
             key = pickle.dumps(pubkey)
-            f = open("key.txt", "w")
-            f.write(str(key))
-            f.close()
             cur.execute("INSERT INTO persons (domain, key ) VALUES (%s,%s)", (domain, str(key)))
             conn.commit()
         i = 0
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import rsa
+import pickle
 
 (pubkey, privkey) = rsa.newkeys(512)
 print(pubkey)
 
 message = b'Hello Blablacode.ru!'
-
 # шифруем
 crypto = rsa.encrypt(message, pubkey)
 print(crypto)
