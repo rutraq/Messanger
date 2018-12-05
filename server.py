c12f71c4e652324333eacf6fa3e993ac0752133d
Phanaty <38048498+Phanaty@users.noreply.github.com>
2018-12-05 14:10:11 +0300

    шифр

--- server.py
+++ server.py
@@ -5,27 +5,36 @@ privkey_str = str(privkey)
 key_str = str(pubkey)
 key_a = key_str[10:164]
 message = input("Message: ")
-print(type(message))
-print(message)
 message = str(message).encode('utf-8')
-print(type(message))
 # шифруем
 key = int(key_a)
-crypto = rsa.encrypt(message, rsa.PublicKey(key, 65537)) #n e
-print(crypto)
+crypto = rsa.encrypt(message, pubkey) #n e
+print('Crypto :',crypto)
+print(type(crypto))
 crypto = str(crypto)
 crypto = crypto[2:-1].encode('utf-8').decode('unicode-escape')
-print("this " + crypto)
-print(type(crypto))
-crypto = crypto.encode('unicode-escape')
-print(crypto)
-print(type(crypto))
-print("Вывод")
+# print("this " + crypto)
+# print(type(crypto))
+# crypto = crypto.encode('unicode-escape')
+# print(crypto)
+# print(type(crypto))
+# print("Вывод")
+mybytes = crypto.encode()
+print('Перевод crypto в bytes',mybytes)
+print(type(mybytes))
+str_mybytes = str(mybytes)
+again_mybytes = eval(str_mybytes)
+decoded = again_mybytes.decode('utf8')
+print(decoded)
+print(type(decoded))
+by = str(decoded).encode()
+print(by)
+print(type(by))
 # d = privkey_str[174:328]
 # d = int(d)
 # p = privkey_str[330:412]
 # p = int(p)
 # q = privkey_str[414:487]
 # q = int(q)
-# inf = rsa.decrypt(crypto, rsa.PrivateKey(key, 65537, d, p, q)) # n e d p q
+# inf = rsa.decrypt(by, privkey) # n e d p q
 # print(inf)
